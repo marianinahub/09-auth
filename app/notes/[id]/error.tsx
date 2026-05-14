9748c305cd46f01827313c24eefe07374e0edb79
Nina Aksenich <maruaninashved1488@gmail.com>
2026-05-14 15:32:00 +0300

    move notes into private routes

--- app/notes/[id]/error.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-type Props = {
-  error: Error & { digest?: string };
-};
-
-export default function NoteDetailsError({ error }: Props) {
-  return <p>Could not fetch note details. {error.message}</p>;
-}
--- app/notes/error.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-type Props = {
-  error: Error & { digest?: string };
-};
-
-export default function NotesError({ error }: Props) {
-  return <p>Could not fetch the list of notes. {error.message}</p>;
-}
